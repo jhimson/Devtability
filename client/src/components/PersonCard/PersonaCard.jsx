01050e4a76b8216ac92f72cacd1a14bee128c7a1
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-26 07:57:14 -0400

    Created reusable person card component

---
+++ client/src/components/PersonCard/PersonaCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Bryce from '../../assets/images/Bryce.png';
+
+const PersonaCard = () => {
+  return (
+    <>
+      <div className="shadow-lg shadow-purple-200 p-4 w-full md:w-96 h-52 rounded bg-gradient-to-bl from-blue-300 to-blue-900 flex flex-col justify-center items-center mt-4 max-w-[24rem]">
+        <img
+          className="w-20 h-20 object-cover rounded-full mb-2"
+          src={Bryce}
+          alt="logo"
+        />
+        <p className="text-gray-100 font-semibold">Bryce Henderson</p>
+        <button className="w-full rounded-md bg-blue-500  py-2 text-indigo-100 hover:bg-blue-700 hover:shadow-md duration-75 mt-8">
+          Add to Contacts
+        </button>
+      </div>
+      {/* <div className="flex items-center justify-center px-4">
+        <div className="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl cursor-pointer">
+          <img
+            src={Bryce}
+            alt="plant"
+            className="h-60 w-full"
+          />
+          <div className="p-5">
+            <p className="text-medium mb-5 text-gray-700 text-center">
+              Bryce Henderson
+            </p>
+            <button className="w-full rounded-md bg-indigo-600  py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">
+              Add to Contacts
+            </button>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default PersonaCard;
