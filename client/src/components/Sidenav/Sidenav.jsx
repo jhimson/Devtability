dd3c2775aac4bfbab230d5afe9d47927bd58cfc8
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-26 07:01:54 -0400

    Added contacts to profile page

--- client/src/components/Sidenav/Sidenav.jsx
+++ client/src/components/Sidenav/Sidenav.jsx
@@ -17,7 +17,7 @@ const Sidenav = () => {
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            {user?.name}
           </h5>
           <span className="hidden text-gray-400 lg:block">User</span>
         </div>
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -141,6 +141,7 @@ const Profile = () => {
           {/* HEREE ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
           <div>
             <article className="">
+              {/* PROFILE START -> */}
               <div className="bg-white shadow rounded-lg p-10 mb-8">
                 <div className="flex flex-col gap-1 text-center items-center">
                   <img
@@ -182,6 +183,80 @@ const Profile = () => {
                   </div>
                 </div>
               </div>
+              {/* PROFILE END -> */}
+
+              {/* CONTACTS STARTS -> */}
+              <div class="bg-white shadow mt-6 rounded-lg p-6">
+                <h3 class="text-gray-600 text-sm font-semibold mb-4 text-center">
+                  Contacts
+                </h3>
+                <ul class="flex items-center justify-center space-x-2">
+                  <li class="flex flex-col items-center space-y-2">
+                    <a class="block bg-white p-1 rounded-full" href="#">
+                      <img
+                        class="w-16 rounded-full"
+                        src="https://images.unsplash.com/photo-1638612913771-8f00622b96fb?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
+                      />
+                    </a>
+                    <span class="text-xs text-gray-500"> Sage </span>
+                  </li>
+
+                  <li class="flex flex-col items-center space-y-2">
+                    <a class="block bg-white p-1 rounded-full" href="#">
+                      <img
+                        class="w-16 rounded-full"
+                        src="https://images.unsplash.com/photo-1638649602320-450b717fa622?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
+                      />
+                    </a>
+
+                    <span class="text-xs text-gray-500"> Jett </span>
+                  </li>
+
+                  <li class="flex flex-col items-center space-y-2">
+                    <a class="block bg-white p-1 rounded-full" href="#">
+                      <img
+                        class="w-16 rounded-full"
+                        src="https://images.unsplash.com/photo-1638708644743-2502f38000a0?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
+                      />
+                    </a>
+
+                    <span class="text-xs text-gray-500"> Sky </span>
+                  </li>
+
+                  <li class="flex flex-col items-center space-y-2">
+                    <a class="block bg-white p-1 rounded-full" href="#">
+                      <img
+                        class="w-16 rounded-full"
+                        src="https://images.unsplash.com/photo-1638691899851-0e955bceba1f?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
+                      />
+                    </a>
+
+                    <span class="text-xs text-gray-500"> Olivia </span>
+                  </li>
+
+                  <li class="flex flex-col items-center space-y-2">
+                    <a class="block bg-white p-1 rounded-full" href="#">
+                      <img
+                        class="w-16 rounded-full"
+                        src="https://images.unsplash.com/photo-1638612913771-8f00622b96fb?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
+                      />
+                    </a>
+
+                    <span class="text-xs text-gray-500"> Julia </span>
+                  </li>
+                  <li class="flex flex-col items-center space-y-2">
+                    <a class="block bg-white p-1 rounded-full" href="#">
+                      <img
+                        class="w-16 rounded-full"
+                        src="https://images.unsplash.com/photo-1638649602320-450b717fa622?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
+                      />
+                    </a>
+
+                    <span class="text-xs text-gray-500"> Hendrick </span>
+                  </li>
+                </ul>
+              </div>
+              {/* CONTACTS END -> */}
               {!isUpdating && (
                 <form
                   onSubmit={handleSubmit}
