46db8ae586a689e061bdbcaed1e7165117e5974d
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-26 09:37:04 -0400

    Created contact context

---
+++ client/src/contexts/ContactContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useState, useContext } from 'react';
+import Axios from 'axios';
+import { UserContext } from './UserContext';
+
+export const ContactContext = createContext();
+
+const ContactContextProvider = ({ children }) => {
+  const { user } = useContext(UserContext);
+
+  const getContacts = async () => {
+    if (localStorage.getItem('contacts'))
+      return JSON.parse(localStorage.getItem('contacts'));
+
+    const res = await Axios.get(
+      `http://localhost:8000/api/contacts/${user._id}`
+    );
+    return res.data;
+  };
+
+  const [contacts, setContacts] = useState(getContacts() || null);
+  return (
+    <ContactContext.Provider value={{ contacts, setContacts }}>
+      {children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactContextProvider;
