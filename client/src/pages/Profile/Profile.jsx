88371a58a6a8546f59f668590d66344c941f5f04
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-26 07:10:29 -0400

    Refactored contacts section and created a separate component to reuse it later

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -20,6 +20,8 @@ import { FaEdit } from 'react-icons/fa';
 import Sidenav from '../../components/Sidenav/Sidenav';
 // ! CONTEXTS IMPORTS
 import { UserContext } from '../../contexts/UserContext';
+import Contacts from '../../components/Contacts/Contacts';
+import ProfileInfo from '../../components/ProfileInfo/ProfileInfo';
 
 const Profile = () => {
   // ! CONTEXTS
@@ -141,121 +143,12 @@ const Profile = () => {
           {/* HEREE ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
           <div>
             <article className="">
-              {/* PROFILE START -> */}
-              <div className="bg-white shadow rounded-lg p-10 mb-8">
-                <div className="flex flex-col gap-1 text-center items-center">
-                  <img
-                    className="h-32 w-32 bg-white p-2 rounded-full shadow mb-4"
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2000&amp;q=80"
-                    alt=""
-                  />
-                  <p className="font-semibold">Jhimson Pamisa</p>
-                  <div className="text-sm leading-normal text-gray-400 flex justify-center items-center mb-8">
-                    <svg
-                      viewBox="0 0 24 24"
-                      className="mr-1"
-                      width="16"
-                      height="16"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                      <circle cx="12" cy="10" r="3"></circle>
-                    </svg>
-                    Endicott, New York
-                  </div>
-                </div>
-                <div className="flex justify-center items-center gap-2 my-3">
-                  <div className="font-semibold text-center mx-4">
-                    <p className="text-black">Email Address</p>
-                    <span className="text-gray-400">jhim.pamisa@gmail.com</span>
-                  </div>
-                  <div className="font-semibold text-center mx-4">
-                    <p className="text-black">Github</p>
-                    <span className="text-gray-400">github.com/Jhimson</span>
-                  </div>
-                  <div className="font-semibold text-center mx-4">
-                    <p className="text-black">LinkedIn</p>
-                    <span className="text-gray-400">LinkedIn.com/Jhimson</span>
-                  </div>
-                </div>
-              </div>
-              {/* PROFILE END -> */}
+              {/* PROFILE INFO START -> */}
+              <ProfileInfo name={user?.name} email={user?.email} />
+              {/* PROFILE INFO END -> */}
 
               {/* CONTACTS STARTS -> */}
-              <div class="bg-white shadow mt-6 rounded-lg p-6">
-                <h3 class="text-gray-600 text-sm font-semibold mb-4 text-center">
-                  Contacts
-                </h3>
-                <ul class="flex items-center justify-center space-x-2">
-                  <li class="flex flex-col items-center space-y-2">
-                    <a class="block bg-white p-1 rounded-full" href="#">
-                      <img
-                        class="w-16 rounded-full"
-                        src="https://images.unsplash.com/photo-1638612913771-8f00622b96fb?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
-                      />
-                    </a>
-                    <span class="text-xs text-gray-500"> Sage </span>
-                  </li>
-
-                  <li class="flex flex-col items-center space-y-2">
-                    <a class="block bg-white p-1 rounded-full" href="#">
-                      <img
-                        class="w-16 rounded-full"
-                        src="https://images.unsplash.com/photo-1638649602320-450b717fa622?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
-                      />
-                    </a>
-
-                    <span class="text-xs text-gray-500"> Jett </span>
-                  </li>
-
-                  <li class="flex flex-col items-center space-y-2">
-                    <a class="block bg-white p-1 rounded-full" href="#">
-                      <img
-                        class="w-16 rounded-full"
-                        src="https://images.unsplash.com/photo-1638708644743-2502f38000a0?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
-                      />
-                    </a>
-
-                    <span class="text-xs text-gray-500"> Sky </span>
-                  </li>
-
-                  <li class="flex flex-col items-center space-y-2">
-                    <a class="block bg-white p-1 rounded-full" href="#">
-                      <img
-                        class="w-16 rounded-full"
-                        src="https://images.unsplash.com/photo-1638691899851-0e955bceba1f?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
-                      />
-                    </a>
-
-                    <span class="text-xs text-gray-500"> Olivia </span>
-                  </li>
-
-                  <li class="flex flex-col items-center space-y-2">
-                    <a class="block bg-white p-1 rounded-full" href="#">
-                      <img
-                        class="w-16 rounded-full"
-                        src="https://images.unsplash.com/photo-1638612913771-8f00622b96fb?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
-                      />
-                    </a>
-
-                    <span class="text-xs text-gray-500"> Julia </span>
-                  </li>
-                  <li class="flex flex-col items-center space-y-2">
-                    <a class="block bg-white p-1 rounded-full" href="#">
-                      <img
-                        class="w-16 rounded-full"
-                        src="https://images.unsplash.com/photo-1638649602320-450b717fa622?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=200&amp;h=200&amp;q=80"
-                      />
-                    </a>
-
-                    <span class="text-xs text-gray-500"> Hendrick </span>
-                  </li>
-                </ul>
-              </div>
+              <Contacts />
               {/* CONTACTS END -> */}
               {!isUpdating && (
                 <form
