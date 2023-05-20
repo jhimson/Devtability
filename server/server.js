7ce55b99dc727dd3fb5450c19baa29300aed30e2
Jhimson <jhimsonrayraboypamisa@gmail.com>
2023-05-20 17:44:26 -0400

    Added CORS settings

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const corsOptions = {
 app.use(fileUpload());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 // app.use(function (req, res, next) {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
